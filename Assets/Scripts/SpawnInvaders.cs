ae8e7d982ce800aede07b2286f8f31faa26fa4b0
muhreeuh <muhreeuh_maria@users.noreply.github.com>
2021-03-30 20:35:54 +0100

    anotha one

--- Assets/Scripts/SpawnInvaders.cs
+++ Assets/Scripts/SpawnInvaders.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnInvaders : MonoBehaviour
 {
 
+
+    
     [SerializeField]
     GameObject invasorA;
 
@@ -12,10 +14,18 @@ public class SpawnInvaders : MonoBehaviour
     GameObject invasorB;
 
     [SerializeField]
+    GameObject invasorC;
+
+    [SerializeField]
     int nInvasores = 7;
 
     [SerializeField]
     float xMin = -3f;
+
+    
+
+
+
 
     void Awake()
     {
@@ -23,12 +33,24 @@ public class SpawnInvaders : MonoBehaviour
          * "Pega" neste objecto, duplica e coloca-o "naquele" sítio
          */
 
-        float x = xMin;
+        
+            float x = xMin;
         for( int i = 1; i <= nInvasores; i++ )
         {
             GameObject newInvader = Instantiate(invasorA, transform);
             newInvader.transform.position = new Vector3(x, -0.5f, 0f);
+            GameObject newInvaderA = Instantiate(invasorA, transform);
+            newInvaderA.transform.position = new Vector3(x, 0f, 0f);
+            GameObject newInvaderB = Instantiate(invasorB, transform);
+            newInvaderB.transform.position = new Vector3(x, 0.5f, 0f);
+            GameObject newInvaderBB = Instantiate(invasorB, transform);
+            newInvaderBB.transform.position = new Vector3(x, 1f, 0f);
+            GameObject newInvaderC = Instantiate(invasorC, transform);
+            newInvaderC.transform.position = new Vector3(x, 1.5f, 0f);
             x += 1f;
+
+            
+            
         }
     }
 }
